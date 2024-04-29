6616da86c49229d26c5c3e79fdd5f496f81ea191
Jay <me@jayxtq.xyz>
2024-04-29 11:00:23 +0100

    feat: add more info

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
-import 'dotenv/config';
-import { ActivityType, Client, GatewayIntentBits } from 'discord.js';
-import path from 'node:path';
-import fs from 'node:fs';
-import { Command } from './types';
-import { NodePgDatabase, drizzle } from 'drizzle-orm/node-postgres';
-import { Client as PGClient } from 'pg';
-import { CronJob } from 'cron';
-import { daily } from './dailies';
-import express, { Response, Request } from 'express';
-import { servers } from '../db/schema';
-import { eq } from 'drizzle-orm';
+import "dotenv/config";
+import { ActivityType, Client, GatewayIntentBits } from "discord.js";
+import path from "node:path";
+import fs from "node:fs";
+import { Command } from "./types";
+import { NodePgDatabase, drizzle } from "drizzle-orm/node-postgres";
+import { Client as PGClient } from "pg";
+import { CronJob } from "cron";
+import { daily } from "./dailies";
+import express, { Response, Request } from "express";
+import { servers } from "../db/schema";
+import { eq } from "drizzle-orm";
 
 const app = express();
 
@@ -21,7 +21,7 @@ export let db: NodePgDatabase;
 
 export const commands = new Map<string, Command>();
 
-client.on('ready', async () => {
+client.on("ready", async () => {
     const pgClient = new PGClient({
         connectionString: process.env.DATABASE_URL,
         ssl: {
@@ -34,51 +34,59 @@ client.on('ready', async () => {
     db = drizzle(pgClient);
 
     for (const file of fs.readdirSync(
-        path.join(process.cwd(), '/src/commands')
+        path.join(process.cwd(), "/src/commands"),
     )) {
-        const command: Command = (await import('./commands/' + file)).default;
+        const command: Command = (await import("./commands/" + file)).default;
         commands.set(command.name, command);
     }
 
     console.log(
         `Bot is in servers with id: ${client.guilds.cache
             .map((guild) => guild.id)
-            .join(', ')}, it has now started`
+            .join(", ")}, it has now started`,
     );
 
     client.user.setPresence({
         activities: [
             {
-                name: 'for /meow',
+                name: "for /meow",
                 type: ActivityType.Watching,
             },
         ],
     });
 });
 
-client.on('interactionCreate', async (interaction) => {
+client.on("interactionCreate", async (interaction) => {
     if (!interaction.isChatInputCommand()) return;
     const command = commands.get(interaction.commandName);
     if (!command) return;
     await command.run(interaction);
 });
 
-client.on('guildDelete', async (guild) => {
+client.on("guildDelete", async (guild) => {
     db.delete(servers).where(eq(servers.id, guild.id));
 });
 
 new CronJob(
-    '0 0 * * *',
+    "0 0 * * *",
     async function () {
         await daily(db, client);
     },
     null,
     true,
-    'utc'
+    "utc",
 ); // i hope this runs honestly :sob:
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('Hello World!');
+app.get("/", async (res: Response) => {
+    res.send(
+        `
+        <link rel="icon" href="${(await client.users.fetch(process.env.CLIENT_ID)).displayAvatarURL()}" />
+<p>Hello World!</p>
+
+<p>There is nothing to look at here, please check out my<a href="https://github.com/JayXTQ/cat-bot">source code</a>!</p>
+
+<p>All images used are owned by their respective owners, I do not own any of them. They are set via the random daily cat image.</p>`,
+    );
 });
 
 app.listen(process.env.PORT || 3000);
